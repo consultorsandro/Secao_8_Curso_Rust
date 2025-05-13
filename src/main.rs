a76f54fa0a3c26345d299886c42ae104466c5942
Sandro Reis <consultorsandro@hotmail.com>
2025-05-13 09:08:53 -0300

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    //Class 132
+    let action_hero = String::from("Silvester Stallone");
+    let first_name: &str = &action_hero[0..9];
+    println!("First name: {}", first_name);
+
+    let last_name  = &action_hero[10..18];
+    println!("Last name: {}", last_name);
+
 }
