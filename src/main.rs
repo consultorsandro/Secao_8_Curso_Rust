fc799f1102911c6c888b3244bce9a8733008fa24
Sandro Reis <consultorsandro@hotmail.com>
2025-05-13 10:18:07 -0300

    Class 135

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 fn main() {
-    // Class 133
+    // Class 135
+    let action_hero = String::from("Silvester Stallone");
+
+    let first_name: &str = &action_hero[..9]; // Considera do início até posição 9
+    println!("His first name is: {}", first_name);
+
+    let last_name: &str = &action_hero[10..]; // considera da posição 10 até a última
+    println!("His last name is: {}", last_name);
+
+    let full_name: &str = &action_hero[..]; // considera do início até o final
+    println!("His full name is: {}", full_name);
+}
+/*
+    // Class 134
     let food = "🍕";
     println!("{}", food.len());
-}
+*/
 /*
     // Class 133
     let first_name = {
