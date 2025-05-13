c7f855832e2963deedba969c9e2feef8235b8576
Sandro Reis <consultorsandro@hotmail.com>
2025-05-13 09:58:51 -0300

    Class 132

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
 fn main() {
+    // Class 133
+    let first_name = {
+        let action_hero = "Silvester Stallone";
+        &action_hero[0..9]
+    };
+    println!("{}", first_name);
+}
+/*
+    // Class 133
+    let first_name = {
+        let action_hero = "Silvester Stallone";
+        &action_hero[0..9]
+    };
+    println!("{}", first_name);
+*/
+/*
     //Class 132
     let action_hero = String::from("Silvester Stallone");
     let first_name: &str = &action_hero[0..9];
@@ -6,5 +22,4 @@ fn main() {
 
     let last_name  = &action_hero[10..18];
     println!("Last name: {}", last_name);
-
-}
+*/
